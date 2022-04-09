4a1bebc6b9a347baaa74228f25b36aea272746e1
Ying <97061798+yingmo55@users.noreply.github.com>
2022-04-09 19:56:07 -0400

    Add style to FeatureCard
    - change display to grid
    - center icon, add padding
    - change text alignment to center

--- src/Components/FeatureCard/FeatureCard.js
+++ src/Components/FeatureCard/FeatureCard.js
@@ -5,9 +5,14 @@ export default function FeatureCard(props) {
     const { src, title, text } = props;
 return (
     <div className='feature-card'>
-        <img src={src} alt={title} />
-        <h3>{title}</h3>
-        <p>{text}</p>
+        <div className='feature-card-icon-container'>
+            <img src={src} alt={title} className='feature-card-icon' />
+        </div>
+        
+        <section className="feature-content">
+            <h3>{title}</h3>
+            <p>{text}</p>
+        </section>
     </div>
 )
 }
