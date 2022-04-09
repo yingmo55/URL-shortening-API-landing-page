812458549eccc3b55cbf601de851abb1d9c5825d
Ying <97061798+yingmo55@users.noreply.github.com>
2022-04-09 19:55:44 -0400

    Add className for component. Add background color

--- src/Components/AdvancedStatistics/AdcancedStatistics.js
+++ src/Components/AdvancedStatistics/AdcancedStatistics.js
@@ -9,12 +9,12 @@ import fullyCustomizableIcon from '../../images/icon-fully-customizable.svg'
 
 export default function AdvancedStatistics() {
     return (
-        <>
+        <section className='advancedStatistics'>
         <h2>Advanced Statistics</h2>
         <p>Track how your links are performing across the web with our advanced statistics dashboard.</p>
         <FeatureCard src={brandRecognitionIcon} title="Brand Recognition" text="Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content." />
         <FeatureCard src={detailedRecordsIcon} title="Detailed Records" text="Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions." />
         <FeatureCard src={fullyCustomizableIcon} title="Fully Customizable" text="Improve brand awareness and content discoverability through customizable links, supercharging audience engagement." />
-        </>
+        </section>
     )
 }
