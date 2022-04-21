8603f10d1b16285e3c1e81c1d4d4a4ef7db3c16f
Ying <97061798+yingmo55@users.noreply.github.com>
2022-04-20 21:26:43 -0400

    Add Mobile Nav bar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../../images/logo.svg';
+import Button from "../Button/Button";
 
 export default function Navbar() {
+
+    const [ showMobileNav, setShowMobileNav ] = useState(false);
+
+    const toggleMobileNav = () => {
+        console.log('it should work')
+        setShowMobileNav(prev => !prev)
+    }
+
     return (
         <>
-        <nav className="mobile-nav">
+        <div className="mobile-nav">
             <img src={logo} alt='shortly logo' />
-            <button className="mobile-burger">
+            <button className="mobile-burger" onClick={toggleMobileNav} >
                 <span className="button-burger"></span>
                 <span className="button-burger"></span>
                 <span className="button-burger"></span>
             </button>
-        </nav>
+            <nav className={ !showMobileNav ? 'nav-menu-hidden' : 'nav-menu' }>
+                <ul>
+                    <li>Features</li>
+                    <li>Pricing</li>
+                    <li>Resources</li>
+                </ul>
+                <hr />
+                <div>
+                    <p>Login</p>
+                    <Button>Sign up</Button>
+                </div>
+            </nav>
+        </div>
         </>
     )
 }
