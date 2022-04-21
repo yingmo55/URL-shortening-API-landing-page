7a72490f597c1f8581881b8674652452c9f56046
Ying <97061798+yingmo55@users.noreply.github.com>
2022-04-20 23:26:48 -0400

    Change href value

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -31,22 +31,22 @@ return (
     <h2>Shortly</h2>
     <p>Features</p>
     <ul>
-        <li><a href="#">Link Shortening</a></li>
-        <li><a href="">Branded Links</a></li>
-        <li><a href="">Analytics</a></li>
+        <li><a href="/#">Link Shortening</a></li>
+        <li><a href="/#">Branded Links</a></li>
+        <li><a href="/#">Analytics</a></li>
     </ul>
     <p>Resources</p>
     <ul>
-        <li><a href="#">blog</a></li>
-        <li><a href="#">Developers</a></li>
-        <li><a href="#">Support</a></li>
+        <li><a href="/#">blog</a></li>
+        <li><a href="/#">Developers</a></li>
+        <li><a href="/#">Support</a></li>
     </ul>
     <p>Company</p>
     <ul>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Our Team</a></li>
-        <li><a href="#">Careers</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="/#">About</a></li>
+        <li><a href="/#">Our Team</a></li>
+        <li><a href="/#">Careers</a></li>
+        <li><a href="/#">Contact</a></li>
     </ul>
     <div className='footerSocialIcons'>
          {
